b7b4fceafdcddef3b12220fbc9de70134deb7e02
liantsoaaa <hei.liantsoa.2@gmail.com>
2025-11-16 14:48:43 +0300

    Modifications de la classe Promotion

--- eval/src/main/java/com/eval/Promotion.java
+++ eval/src/main/java/com/eval/Promotion.java
@@ -1,13 +1,29 @@
 package com.eval;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.List;
 
 public class Promotion {
-    private Map<String, Etudiants> groupe;
+    private String nom;
+    private List<String> groupes;
 
-    public Promotion(Map<String, Etudiants> groupe) {
-        this.groupe = groupe;
-    }   
+    public Promotion(String nom, List<String> groupes) {
+        this.nom = nom;
+        this.groupes = groupes;
+    }
 
+    public String getNom() {
+        return nom;
+    }
+    public List<String> getGroupes() {
+        return groupes;
+    }
+
+    public void addGroupe(String groupe) {
+        this.groupes.add(groupe);
+    }
+
+    @Override
+    public String toString() {
+        return "Promotion : " + nom + ", groupes = " + groupes;
+    }
 }
